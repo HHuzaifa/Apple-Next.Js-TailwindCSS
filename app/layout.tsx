1876764f37f98d8fd9cbcea4978573891fd51954
HHuzaifa <90773074+HHuzaifa@users.noreply.github.com>
2023-06-09 23:51:08 +0500

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import './globals.css'
+import "./globals.css";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 export const metadata = {
-  title: 'Apple',
-  description: 'A clone of Apple product website',
-}
+  title: "Apple",
+  description: "A clone of Apple product website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <Header />
       <body>{children}</body>
+      <Footer />
     </html>
-  )
+  );
 }
