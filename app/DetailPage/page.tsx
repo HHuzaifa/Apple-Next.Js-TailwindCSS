591c0dd21b3139e0f3b5bd317724a20d1d6ae2d3
HHuzaifa <90773074+HHuzaifa@users.noreply.github.com>
2023-06-12 13:47:49 +0500

    .

---
+++ app/DetailPage/page.tsx
@@ -0,0 +1,134 @@
+export default function DetailPage() {
+  return (
+    <section className="w-full">
+      <div className="flex flex-col items-center justify-center py-28">
+        <h1 className="text-2xl md:text-4xl lg:text-7xl text-center font-extrabold py-28">
+          Which AirPods are for you?
+        </h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-x-28 lg:grid-cols-4 lg:gap-x-36 justify-center items-center text-center">
+          <div className="flex flex-col justify-center items-center w-36">
+            <img src="/detailPage/dp1.png"></img>
+            <p className="text-xl pt-4 font-bold">AirPods</p>
+            <button>
+              <img
+                className="w-14 py-2"
+                src="/buyButton.png"
+                alt="buy button"
+              ></img>
+            </button>
+            <button>
+              {" "}
+              <img className="w-16" src="/LearnMore.png" alt="learn more"></img>
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center w-36">
+            <img src="/detailPage/dp2.png"></img>
+            <p className="text-xl pt-4 font-bold">AirPods</p>
+            <button>
+              <img
+                className="w-14 py-2"
+                src="/buyButton.png"
+                alt="buy button"
+              ></img>
+            </button>
+            <button>
+              {" "}
+              <img className="w-16" src="/LearnMore.png" alt="learn more"></img>
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center w-36">
+            <img src="/detailPage/dp4.png"></img>
+            <p className="text-xl pt-4 font-bold">AirPods</p>
+            <button>
+              <img
+                className="w-14 py-2"
+                src="/buyButton.png"
+                alt="buy button"
+              ></img>
+            </button>
+            <button>
+              {" "}
+              <img className="w-16" src="/LearnMore.png" alt="learn more"></img>
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center w-36">
+            <img src="/detailPage/dp3.png"></img>
+            <p className="text-xl pt-4 font-bold">AirPods</p>
+            <button>
+              <img
+                className="w-14 py-2"
+                src="/buyButton.png"
+                alt="buy button"
+              ></img>
+            </button>
+            <button>
+              {" "}
+              <img className="w-16" src="/LearnMore.png" alt="learn more"></img>
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center w-36">
+            <img src="/detailPage/dp3.png"></img>
+            <p className="text-xl pt-4 font-bold">AirPods</p>
+            <button>
+              <img
+                className="w-14 py-2"
+                src="/buyButton.png"
+                alt="buy button"
+              ></img>
+            </button>
+            <button>
+              {" "}
+              <img className="w-16" src="/LearnMore.png" alt="learn more"></img>
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center w-36">
+            <img src="/detailPage/dp4.png"></img>
+            <p className="text-xl pt-4 font-bold">AirPods</p>
+            <button>
+              <img
+                className="w-14 py-2"
+                src="/buyButton.png"
+                alt="buy button"
+              ></img>
+            </button>
+            <button>
+              {" "}
+              <img className="w-16" src="/LearnMore.png" alt="learn more"></img>
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center w-36">
+            <img src="/detailPage/dp2.png"></img>
+            <p className="text-xl pt-4 font-bold">AirPods</p>
+            <button>
+              <img
+                className="w-14 py-2"
+                src="/buyButton.png"
+                alt="buy button"
+              ></img>
+            </button>
+            <button>
+              {" "}
+              <img className="w-16" src="/LearnMore.png" alt="learn more"></img>
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center w-36">
+            <img src="/detailPage/dp1.png"></img>
+            <p className="text-xl pt-4 font-bold">AirPods</p>
+            <button>
+              <img
+                className="w-14 py-2"
+                src="/buyButton.png"
+                alt="buy button"
+              ></img>
+            </button>
+            <button>
+              {" "}
+              <img className="w-16" src="/LearnMore.png" alt="learn more"></img>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AirPodMax from "./AirPodMax/page";
 import AirPods from "./AirPods/page";
+import DetailPage from "./DetailPage/page";
 
 export default function Home() {
   return (
     <main>
       <AirPodMax />
       <AirPods />
+      <DetailPage />
     </main>
   )
 }
