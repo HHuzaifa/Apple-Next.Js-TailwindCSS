cb108edffe34825c0b9b1037f57199246d89560a
HHuzaifa <90773074+HHuzaifa@users.noreply.github.com>
2023-06-12 13:47:46 +0500

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
+
 export default function Footer() {
     return (
-        <footer>
-            
-        </footer>
+        <section className="w-full flex-col justify-center bg-black">
+                <div className="w-full flex flex-col items-center lg:flex-row justify-around gap-x-96">
+                    <div className="flex text-white text-xl lg:text-2xl space-x-5 pb-3 lg:pb-0">
+                        <img className="w-8" src="appleLogoWhite.png" ></img>
+                    </div>
+                    <h6 className="text-white">© 2023 Apple. All rights reserved</h6>
+                </div>
+        </section>
     )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { BsBag } from "react-icons/bs";
@@ -11,7 +11,7 @@ export default function Header() {
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    const handleClickOutside = (event:any) => {
+    const handleClickOutside = (event: any) => {
       if (open && !event.target.closest(".mobile-nav")) {
         setOpen(false);
       }
@@ -21,18 +21,14 @@ export default function Header() {
   }, [open]);
 
   return (
-    <header>
+    <header className="inset-0 sticky z-40">
       {/* Mobile Nav Menu */}
       <div className="lg:hidden h-14 bg-white inset-0 fixed z-50">
         <Wrapper>
           <div className="flex justify-between items-center sticky inset-0 h-16">
             <div className="flex justify-between items-center">
               <div className="relative h-10 w-10">
-                <Image
-                  src={"/apple-logo.png"}
-                  fill={true}
-                  alt="Apple Logo"
-                />
+                <Image src={"/apple-logo.png"} fill={true} alt="Apple Logo" />
               </div>
             </div>
 
@@ -42,33 +38,71 @@ export default function Header() {
             </div>
 
             <div onClick={() => setOpen(!open)}>
-              {open ? (
-                <TfiClose size={20} />
-              ) : (
-                <SlMenu size={20} />
-              )}
+              {open ? <TfiClose size={20} /> : <SlMenu size={20} />}
             </div>
           </div>
         </Wrapper>
 
         {open && (
           <ul className="absolute top-14 transition-opacity duration-300 w-full mobile-nav  flex flex-col items-center shadow-lg gap-y-5 rounded py-7 bg-black bg-opacity-80 backdrop-blur">
-            <li className="text-base text-white font-normal hover:font-bold">Store</li>
-            <li className="text-base text-white font-normal hover:font-bold">Mac</li>
-            <li className="text-base text-white font-normal hover:font-bold">iPad</li>
-            <li className="text-base text-white font-normal hover:font-bold">iPhone</li>
-            <li className="text-base text-white font-normal hover:font-bold">Watch</li>
-            <li className="text-base text-white font-normal hover:font-bold">AirPods</li>
-            <li className="text-base text-white font-normal hover:font-bold">
-              TV & Home
-            </li>
-            <li className="text-base text-white font-normal hover:font-bold">
-              Entertainment
-            </li>
-            <li className="text-base text-white font-normal hover:font-bold">
-              Accessories
-            </li>
-            <li className="text-base text-white font-normal hover:font-bold">Support</li>
+            <button>
+              <li className="text-base text-white font-normal hover:font-bold">
+                Store
+              </li>
+            </button>
+            <button>
+              {" "}
+              <li className="text-base text-white font-normal hover:font-bold">
+                Mac
+              </li>
+            </button>
+            <button>
+              {" "}
+              <li className="text-base text-white font-normal hover:font-bold">
+                iPad
+              </li>
+            </button>
+            <button>
+              {" "}
+              <li className="text-base text-white font-normal hover:font-bold">
+                iPhone
+              </li>
+            </button>
+            <button>
+              {" "}
+              <li className="text-base text-white font-normal hover:font-bold">
+                Watch
+              </li>
+            </button>
+            <button>
+              {" "}
+              <li className="text-base text-white font-normal hover:font-bold">
+                AirPods
+              </li>
+            </button>
+            <button>
+              {" "}
+              <li className="text-base text-white font-normal hover:font-bold">
+                TV & Home
+              </li>
+            </button>
+            <button>
+              <li className="text-base text-white font-normal hover:font-bold">
+                Entertainment
+              </li>
+            </button>
+            <button>
+              {" "}
+              <li className="text-base text-white font-normal hover:font-bold">
+                Accessories
+              </li>
+            </button>
+            <button>
+              {" "}
+              <li className="text-base text-white font-normal hover:font-bold">
+                Support
+              </li>
+            </button>
           </ul>
         )}
       </div>
@@ -79,38 +113,54 @@ export default function Header() {
 
       {/* Desktop Nav Menu */}
 
-      <div className="hidden lg:sticky lg:inset-0 lg:flex justify-center items-center gap-x-8 h-20">
-
-
+      <div className="hidden bg-white lg:sticky lg:inset-0 lg:flex justify-center items-center gap-x-8 h-20">
         <div className="relative w-10 h-10">
           <Image src={"/apple-logo.png"} fill={true} alt="Apple Logo" />
         </div>
 
-
         <ul className="flex gap-x-8">
-          <li className="text-base font-normal ">Store</li>
-          <li className="text-base font-normal">Mac</li>
-          <li className="text-base font-normal">iPad</li>
-          <li className="text-base font-normal">iPhone</li>
-          <li className="text-base font-normal">Watch</li>
-          <li className="text-base font-normal">AirPods</li>
-          <li className="text-base font-normal">TV & Home</li>
-          <li className="text-base font-normal">Entertainment</li>
-          <li className="text-base font-normal">Accessories</li>
-          <li className="text-base font-normal">Support</li>
+          <button>
+            <li className="text-base font-normal ">Store</li>
+          </button>
+          <button>
+            {" "}
+            <li className="text-base font-normal">Mac</li>
+          </button>
+          <button>
+            <li className="text-base font-normal">iPad</li>
+          </button>
+          <button>
+            <li className="text-base font-normal">iPhone</li>
+          </button>
+          <button>
+            <li className="text-base font-normal">Watch</li>
+          </button>
+          <button>
+            <li className="text-base font-normal">AirPods</li>
+          </button>
+          <button>
+            {" "}
+            <li className="text-base font-normal">TV & Home</li>
+          </button>
+          <button>
+            {" "}
+            <li className="text-base font-normal">Entertainment</li>
+          </button>
+          <button>
+            {" "}
+            <li className="text-base font-normal">Accessories</li>
+          </button>
+          <button>
+            {" "}
+            <li className="text-base font-normal">Support</li>
+          </button>
         </ul>
-
 
         <div className="flex gap-x-8">
           <CiSearch size={25} />
           <BsBag size={21} />
         </div>
-
-
       </div>
-
-
-
     </header>
   );
 }
